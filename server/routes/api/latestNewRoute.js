89f51135d8bcf8f7b86af5f7325bdd21bd169b14
Shoaib Ansari <mohd.shoaib0118@gmail.com>
2025-09-06 17:26:10 +0530

    implement api of contact

--- server/routes/api/latestNewRoute.js
+++ server/routes/api/latestNewRoute.js
@@ -1,17 +1,17 @@
 // routes/latestNewsRoutes.js
 const express = require('express');
-const { createLatestNews,getAllLatestNews,getLatestNewsById,updateLatestNews,deleteLatestNews,
- } = require('../../controller/latestNewController');
+const { createLatestNews, getAllLatestNews, getLatestNewsById, updateLatestNews, deleteLatestNews,
+} = require('../../controller/latestNewController');
 const upload = require('../../middleware/multer');
 const { verifyToken } = require('../../middleware/authMiddleware');
 
 
 const router = express.Router();
 
-router.post('/add',verifyToken, upload.single('image'), createLatestNews);
+router.post('/add', verifyToken, upload.single('image'), createLatestNews);
 router.get('/getAllLatestNews', getAllLatestNews);
 router.get('/getLatestNewsById/:id', getLatestNewsById);
-router.put('/edit/:id', verifyToken,upload.single('image'), updateLatestNews); 
-router.delete('/delete',verifyToken, deleteLatestNews );
+router.put('/edit', verifyToken, upload.single('image'), updateLatestNews);
+router.delete('/delete', verifyToken, deleteLatestNews);
 
 module.exports = router;
